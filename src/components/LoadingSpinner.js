6873ce606950b1e500d455b8134ffbaca4f9559f
Lucas Ferreira <luscafreis@gmail.com>
2024-08-22 23:56:23 -0300

    Adicionando Compomente de loading

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoadingSpinner() {
+    return (
+        <div className="spinner-border text-primary" role="status">
+            <span className="sr-only"></span>
+        </div>
+    );
+}
+
+export default LoadingSpinner;
